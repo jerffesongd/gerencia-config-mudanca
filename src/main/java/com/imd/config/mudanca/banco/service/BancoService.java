452bdb4a93f19d0a2ecd7a2d2c8223b1a3df254c
Jerffeson Gomes <jerffesongd@gmail.com>
2020-10-12 09:48:38 -0300

    #5 - dividindo verificação de ocnta e saldo

--- src/main/java/com/imd/config/mudanca/banco/service/BancoService.java
+++ src/main/java/com/imd/config/mudanca/banco/service/BancoService.java
@@ -12,35 +12,41 @@ import com.imd.config.mudanca.banco.helper.Mensagens;
 public class BancoService {
 
 	
-	public void realizarSaque(Conta conta, BigDecimal valor) {
+	public void visualizarSaldo(Conta conta, BigDecimal valor) {
 		
-		validarOperacaoSaque(conta, valor);
+		validarConta(conta);
+		
 		
 	}
 
 	private void validarOperacaoSaque(Conta conta, BigDecimal valor) {
 		
-		if (conta == null) {
-			throw new InvalidParameterException(Mensagens.CONTA_NAO_EXISTE);
-		}
+		validarConta(conta);
 		
 		if (valor == null) {
 			throw new InvalidParameterException(Mensagens.VALOR_TRANSACAO_INVALIDO);
 		}
 		
-		
 		if (valor.compareTo(new BigDecimal("0")) == 0 || valor.compareTo(new BigDecimal("0"))  == -1) {
 			throw new InvalidParameterException(Mensagens.VALOR_TRANSACAO_INVALIDO);
 		}
 		
-		if (!contaExiste(conta)) {
-			throw new InvalidParameterException(Mensagens.CONTA_NAO_EXISTE);
-		}
+		
 		
 		if (conta.getSaldo().compareTo(valor) == -1) {
 			throw new InvalidParameterException(Mensagens.SALDO_INSUFICIENTE);
 		}
 		
+	}
+
+	private void validarConta(Conta conta) {
+		if (conta == null) {
+			throw new InvalidParameterException(Mensagens.CONTA_NAO_EXISTE);
+		}
+		
+		if (!contaExiste(conta)) {
+			throw new InvalidParameterException(Mensagens.CONTA_NAO_EXISTE);
+		}
 	}
 
 	private boolean contaExiste(Conta conta) {
