f770c1b7e4fcf56f4a742cf728188b63a8ce535f
Joicy Oliveira <joiicyoliv@gmail.com>
2020-10-11 21:07:14 -0300

    #9 - Alterando o tipo da variável valor

--- src/main/java/com/imd/config/mudanca/banco/command/Operacao.java
+++ src/main/java/com/imd/config/mudanca/banco/command/Operacao.java
@@ -1,8 +1,10 @@
 package com.imd.config.mudanca.banco.command;
 
+import java.math.BigDecimal;
+
 import com.imd.config.mudanca.banco.domain.Conta;
 
 public interface Operacao {
 
-		public double executar(Conta contaOrigem, Conta contaDestino, int valor);
+		public double executar(Conta contaOrigem, Conta contaDestino, BigDecimal valor);
 }
--- src/main/java/com/imd/config/mudanca/banco/command/Transferencia.java
+++ src/main/java/com/imd/config/mudanca/banco/command/Transferencia.java
@@ -1,11 +1,13 @@
 package com.imd.config.mudanca.banco.command;
 
+import java.math.BigDecimal;
+
 import com.imd.config.mudanca.banco.domain.Conta;
 
 public class Transferencia implements Operacao{
 
 	@Override
-	public double executar(Conta contaOrigem, Conta contaDestino, int valor) {
+	public double executar(Conta contaOrigem, Conta contaDestino, BigDecimal valor) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
